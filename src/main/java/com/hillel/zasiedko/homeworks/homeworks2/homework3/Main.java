27175a1de1e1b5d52b5d5fa902ad9d1965a7dad1
zasiedkoarsen <arsenzasedko67@gmail.com>
2022-10-21 19:12:03 +0300

    commit all files

---
+++ src/main/java/com/hillel/zasiedko/homeworks/homeworks2/homework3/Main.java
@@ -0,0 +1,21 @@
+package com.hillel.zasiedko.homeworks.homeworks2.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        char age = '\u004A';
+        System.out.println(age);
+
+//        latitude coordinates
+        double width = 50.4908696;
+
+//        longitude coordinates
+        double longitude = 30.5004314;
+        char degree = '\u00BA';
+        System.out.println("width = " + width + degree + "; longitude = " + longitude + degree);
+
+    }
+
+}
+
