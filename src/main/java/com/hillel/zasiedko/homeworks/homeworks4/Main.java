3debcb622f128fdcabde89100fa44dbf799c518b
zasiedkoarsen <arsenzasedko67@gmail.com>
2022-10-24 22:23:56 +0300

    Homeworks 4 (1)

---
+++ src/main/java/com/hillel/zasiedko/homeworks/homeworks4/Main.java
@@ -0,0 +1,19 @@
+package com.hillel.zasiedko.homeworks.homeworks4;
+
+public class Main {
+    public static void main(String[] args) {
+        int front = 5;
+        int back = 6;
+        int FromTheSide = 8;
+        int volume = front * back * FromTheSide;
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        int length = 4 * (front + back + FromTheSide);
+        System.out.println(length);
+
+    }
+
+
+
+
+
+}
