5cb4ade63109f2a42c6e4bf8c147305c2a01ba95
zasiedkoarsen <arsenzasedko67@gmail.com>
2022-12-12 17:34:54 +0200

    Homeworks 13

--- src/main/java/com/hillel/zasiedko/homeworks/homeworks13/Main.java
+++ src/main/java/com/hillel/zasiedko/homeworks/homeworks13/Main.java
@@ -4,14 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Androids androids = new Androids("Android");
-        Iphones iphones = new Iphones("Iphone");
 
         androids.phonesInfo();
         androids.sms();
         androids.call();
         androids.internet();
         androids.LinuxOS();
-        System.out.println();
+
+        Iphones iphones = new Iphones("Iphone");
+
         iphones.phonesInfo2();
         iphones.sms();
         iphones.call();
